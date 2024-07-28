61f6b830761283bb91673ee589ffa49fd46471fb
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-07-28 22:18:54 +0700

    test: anomali 😞

--- pages/page/index.vue
+++ pages/page/index.vue
@@ -384,11 +384,12 @@ a {
 }
 
 .page .tabel-container .tabel .card .card-head {
-    width: fit-content;
+    width: 50%;
     height: 100%;
     display: flex;
     align-items: center;
     margin-left: 20px;
+    overflow: hidden;
 }
 
 .page .tabel-container .tabel .card .card-head .thumbnail {
